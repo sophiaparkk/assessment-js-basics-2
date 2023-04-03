1cd48b26cc0a17b3aba3e6348f48c2970e5d8586
Sophia Park <park.sophiakela@gmail.com>
2023-04-03 16:01:26 -0600

    Orders complete

--- orders.js
+++ orders.js
@@ -31,6 +31,20 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor(items, orderTime, customerID){
+        this.items = items
+        this.orderTime = orderTime
+        this.customerID = customerID
+        this.status = {value:'queued'}
+    }
+
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log('The order for customer ', this.customerID, 'is now ', this.status)
+    }
+}
+
 
 
 /*
@@ -46,6 +60,8 @@
 
 //CODE HERE
 
+let firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 pm', 575)
+
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +70,5 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking')
